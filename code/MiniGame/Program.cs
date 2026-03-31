02a476dbe0d682da198f8fa1d90ad3246afb4466
ppinedo-hiberuscom <ppinedo@hiberus.com>
2026-03-31 07:53:36 +0000

    Agregar función CalculateNumbers para realizar operaciones matemáticas básicas

--- code/MiniGame/Program.cs
+++ code/MiniGame/Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Ready to build a mini game with .NET/C# with help from GitHub Copilot?");
+void CalculateNumbers(int var1, int var2)
+
+{
+    int sum = var1 + var2;
+    int difference = var1 - var2;
+    int product = var1 * var2;
+    int quotient = var1 / var2;
+
+    Console.WriteLine($"The sum of {var1} and {var2} is: {sum}");
+    Console.WriteLine($"The difference of {var1} and {var2} is: {difference}");
+    Console.WriteLine($"The product of {var1} and {var2} is: {product}");
+    Console.WriteLine($"The quotient of {var1} and {var2} is: {quotient}");
+}
